579af7766c62eb59ccddfd6a06d847722a20fed9
3VNXP3VCA1PTYEN\Administrator <1154651663@qq.com>
2018-03-25 01:32:43 +0800

    新增订单列表页面

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -1,11 +1,11 @@
 require('./config$');
+require('../..//pages/order/order');
 require('../..//pages/index/index');
 require('../..//pages/order/order-sure/order-sure');
 require('../..//pages/me/me-addr/me-addr');
 require('../..//pages/me/me-addr-edit/me-addr-edit');
 require('../..//pages/order/order-remark/order-remark');
 require('../..//pages/order/pay-order/pay-order');
-require('../..//pages/order/order');
 require('../..//pages/me/me');
 require('../..//pages/todos/todos');
 require('../..//pages/add-todo/add-todo');
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,3 +1,10 @@
 Page({
-    data:{name:'hellow'}
+    data:{
+        orders:[
+            {id:1001,title:"套餐鸡蛋+油条+面包 丰盛的早餐 哇哈哈哈",ordreState:"订单已完成",orderTime:"2018-04-12 08:30:22",ortherRemark:"油条等2件商品",pay:"20",pic:"/img/img_item_default.png"},
+            {id:1002,title:"套餐鸡蛋+油条+面包",ordreState:"订单已支付",orderTime:"2018-04-12 08:30:22",ortherRemark:"油条等2件商品",pay:"20",pic:"/img/img_item_default.png"},
+            {id:1003,title:"套餐鸡蛋+油条+面包 丰盛的早餐 哇哈哈哈",ordreState:"订单已取消",orderTime:"2018-04-12 08:30:22",ortherRemark:"油条等2件商品",pay:"20",pic:"/img/img_item_default.png"},
+            {id:1004,title:"套餐鸡蛋+油条+面包",ordreState:"订单派送中",orderTime:"2018-04-12 08:30:22",ortherRemark:"油条等2件商品油条等2件商品油条等2件商品",pay:"20",pic:"/img/img_item_default.png"},
+        ]
+    }
 });
